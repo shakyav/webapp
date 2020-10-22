e50587bc05a402197975e35ac8e81a2fdc5b946f
Vivek Shakya <shakya.v@northeastern.edu>
2020-10-22 02:33:14 -0400

    access env removed

--- app/appConfig/s3.env.js
+++ app/appConfig/s3.env.js
@@ -1,7 +1,7 @@
 	
 const env_vars = {
-    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY, // change to yours
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_KEY, // change to yours
+    AWS_ACCESS_KEY: "", // change to yours
+    AWS_SECRET_ACCESS_KEY: "", // change to yours
     REGION : process.env.REGION, // change to yours
     Bucket: process.env.Bucket_Name // change to yours
   };
